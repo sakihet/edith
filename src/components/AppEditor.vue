55d78ce9286600b181316028bc0208344db49c8c
saki <sakihet@gmail.com>
2024-06-07 09:43:41 +0900

    add AppEditor

---
+++ src/components/AppEditor.vue
@@ -0,0 +1,26 @@
+<script>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  setup() {
+    const editor = useEditor({
+      content: '<p>hello, editor</p>',
+      extensions: [
+        StarterKit,
+      ],
+    })
+
+    return {editor}
+  },
+}
+
+</script>
+
+<template>
+  <EditorContent :editor="editor" />
+</template>
