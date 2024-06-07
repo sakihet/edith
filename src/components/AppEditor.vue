ed05609c8f70d80ccd9e483f142098675faaada5
saki <sakihet@gmail.com>
2024-06-07 13:14:52 +0900

    add Bold

--- src/components/AppEditor.vue
+++ src/components/AppEditor.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { useEditor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
+import Bold from '@tiptap/extension-bold'
 import BulletList from '@tiptap/extension-bullet-list'
 
 const editor = useEditor({
   content: '<p>hello, editor</p>',
   extensions: [
+    Bold,
     BulletList,
     StarterKit,
   ],
@@ -15,7 +17,16 @@ const editor = useEditor({
 <template>
   <div class="layout-stack-4">
     <div>
-      <button @click="editor?.chain().focus().toggleBulletList().run()">toggle bullet list</button>
+      <button
+        @click="editor?.chain().focus().toggleBulletList().run()"
+      >
+        toggle bullet list
+      </button>
+      <button
+        @click="editor?.chain().focus().toggleBold().run()"
+      >
+        toggle bold
+      </button>
     </div>
     <EditorContent :editor="editor" />
   </div>
