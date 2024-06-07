be9dc6b6701a2f32cd84f7f156faa6b001bba51a
saki <sakihet@gmail.com>
2024-06-07 13:09:04 +0900

    script setup

--- src/components/AppEditor.vue
+++ src/components/AppEditor.vue
@@ -1,32 +1,21 @@
-<script>
+<script setup lang="ts">
 import { useEditor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
 import BulletList from '@tiptap/extension-bullet-list'
 
-export default {
-  components: {
-    EditorContent,
-  },
-
-  setup() {
-    const editor = useEditor({
-      content: '<p>hello, editor</p>',
-      extensions: [
-        BulletList,
-        StarterKit,
-      ],
-    })
-
-    return {editor}
-  },
-}
-
+const editor = useEditor({
+  content: '<p>hello, editor</p>',
+  extensions: [
+    BulletList,
+    StarterKit,
+  ],
+})
 </script>
 
 <template>
   <div class="layout-stack-4">
     <div>
-      <button @click="editor.chain().focus().toggleBulletList().run()">toggle bullet list</button>
+      <button @click="editor?.chain().focus().toggleBulletList().run()">toggle bullet list</button>
     </div>
     <EditorContent :editor="editor" />
   </div>
