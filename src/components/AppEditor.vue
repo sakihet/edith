0e6b6a7b9a8a99546a4142980097ec57fc5058e7
saki <sakihet@gmail.com>
2024-06-07 13:53:56 +0900

    add BubbleMenu

--- src/components/AppEditor.vue
+++ src/components/AppEditor.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { useEditor, EditorContent } from '@tiptap/vue-3'
+import { useEditor, EditorContent, BubbleMenu } from '@tiptap/vue-3'
 import Bold from '@tiptap/extension-bold'
+import { BubbleMenu as BubbleMenuExt } from '@tiptap/extension-bubble-menu'
 import BulletList from '@tiptap/extension-bullet-list'
 import Link from '@tiptap/extension-link'
 import StarterKit from '@tiptap/starter-kit'
@@ -9,6 +10,7 @@ const editor = useEditor({
   content: '<p>hello, editor</p>',
   extensions: [
     Bold,
+    BubbleMenuExt,
     BulletList,
     Link,
     StarterKit,
@@ -18,6 +20,18 @@ const editor = useEditor({
 
 <template>
   <div class="layout-stack-4">
+    <div>
+      <BubbleMenu
+        :editor="editor"
+        v-if="editor"
+      >
+        <button
+          @click="editor?.chain().focus().toggleBold().run()"
+        >
+          bold
+        </button>
+      </BubbleMenu>
+    </div>
     <div>
       <button
         @click="editor?.chain().focus().toggleBulletList().run()"
