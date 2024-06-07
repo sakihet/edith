07308ae07c57d6c66c53c278011d1791004f4fe5
saki <sakihet@gmail.com>
2024-06-07 13:30:59 +0900

    add Link

--- src/components/AppEditor.vue
+++ src/components/AppEditor.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { useEditor, EditorContent } from '@tiptap/vue-3'
-import StarterKit from '@tiptap/starter-kit'
 import Bold from '@tiptap/extension-bold'
 import BulletList from '@tiptap/extension-bullet-list'
+import Link from '@tiptap/extension-link'
+import StarterKit from '@tiptap/starter-kit'
 
 const editor = useEditor({
   content: '<p>hello, editor</p>',
   extensions: [
     Bold,
     BulletList,
+    Link,
     StarterKit,
   ],
 })
@@ -26,6 +28,12 @@ const editor = useEditor({
         @click="editor?.chain().focus().toggleBold().run()"
       >
         toggle bold
+      </button>
+      <button
+        @click="editor?.chain().focus().unsetLink().run()"
+        :disabled="!editor?.isActive('link')"
+      >
+        unset link
       </button>
     </div>
     <EditorContent :editor="editor" />
