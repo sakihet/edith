f04d6bbaa52e679f944636a6a27912a00aa68a62
saki <sakihet@gmail.com>
2024-07-30 23:12:33 +0900

    update for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Edith',
+        short_name: 'Edith',
+        description: 'Just a Note-Taking PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      registerType: 'autoUpdate'
+    }),
     vue()
   ],
 })
