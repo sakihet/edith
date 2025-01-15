a171226fc32a8a9904d8bc14f14fa36c39258e8d
saki <sakihet@gmail.com>
2025-01-15 23:20:04 +0900

    update test

--- tests/store/index.test.ts
+++ tests/store/index.test.ts
@@ -29,4 +29,64 @@ describe('store', () => {
     await store.clear()
     expect(store.notes.length).toBe(0)
   })
+  it('delete', async () => {
+    const now = new Date().toISOString()
+    const n: Note = {
+      id: v4(),
+      content: {
+        "type": "doc",
+        "content": []
+      },
+      createdAt: now,
+      updatedAt: now
+    }
+    await store.add(n)
+    expect(store.notes.length).toBe(1)
+    await store.delete(n.id)
+    expect(store.notes.length).toBe(0)
+  })
+  it('put', async () => {
+    const now = new Date().toISOString()
+    const n: Note = {
+      id: v4(),
+      content: {
+        type: "doc",
+        content: [
+          {
+            type: "paragraph",
+            content: [
+              {
+                text: "hello",
+                type: "text"
+              }
+            ]
+          }
+        ]
+      },
+      createdAt: now,
+      updatedAt: now
+    }
+    await store.add(n)
+    expect(store.notes.length).toBe(1)
+    const updatedParagraph = {
+      type: "paragraph",
+      content: [
+        {
+          text: "world",
+          type: "text"
+        }
+      ]
+    }
+    const updated = {
+      ...n,
+      content: {
+        type: "doc",
+        content: [
+          updatedParagraph
+        ]
+      }
+    }
+    await store.put(updated)
+    expect(store.notes[0] && store.notes[0].content.content[0].content[0].text).toBe("world")
+  })
 })
