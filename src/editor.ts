cb052c7d408e632688087035ea0fe786e0efa323
saki <sakihet@gmail.com>
2025-02-19 22:09:02 +0900

    use debounce when store.put

--- src/editor.ts
+++ src/editor.ts
@@ -1,4 +1,4 @@
-import { Editor, generateText } from "@tiptap/vue-3"
+import { generateText } from "@tiptap/vue-3"
 import Blockquote from '@tiptap/extension-blockquote'
 import Bold from '@tiptap/extension-bold'
 import { BubbleMenu as BubbleMenuExt } from '@tiptap/extension-bubble-menu'
@@ -21,6 +21,8 @@ import TaskList from '@tiptap/extension-task-list'
 import Underline from '@tiptap/extension-underline'
 import Youtube from "@tiptap/extension-youtube"
 import Text from '@tiptap/extension-text'
+import { useDebounceFn } from '@vueuse/core'
+import { Editor } from '@tiptap/core'
 
 import Commands from './commands'
 import suggestion from './suggestion'
@@ -65,15 +67,19 @@ const extensions = [
 
 export const generateTextCustom = (json: any) => generateText(json, extensions)
 
+const debouncedFn = useDebounceFn((store: Store, note: Note, editor: Editor) => {
+  store.put({
+    id: note.id,
+    content: editor.getJSON(),
+    createdAt: note.createdAt,
+    updatedAt: new Date().toISOString()
+  })
+}, 1000)
+
 export const createEditor = (note: Note, store:Store) => new Editor({
   content: note.content,
   extensions: extensions,
   onUpdate({ editor }) {
-    store.put({
-      id: note.id,
-      content: editor.getJSON(),
-      createdAt: note.createdAt,
-      updatedAt: new Date().toISOString()
-    })
+    debouncedFn(store, note, editor)
   }
 })
